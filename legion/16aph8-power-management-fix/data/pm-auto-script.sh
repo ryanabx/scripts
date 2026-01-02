1c197757f42d3e6ced0cf9b123cf620f532dffe6
Ryan Brue <ryanbrue.dev@gmail.com>
2026-01-02 01:30:01 -0600

    WIP legion power management fix
    Signed-off-by: Ryan Brue <ryanbrue.dev@gmail.com>

---
+++ legion/16aph8-power-management-fix/data/pm-auto-script.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+# Set nvidia runtime power management to auto
+echo auto > /sys/bus/pci/devices/0000:01:00.0/power/control
---
+++ legion/16aph8-power-management-fix/install.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+# https://github.com/NVIDIA/open-gpu-kernel-modules/issues/905#issuecomment-3360970555
+
+# Install modprobe config
+sudo install -Dm0644 data/modprobe-config.conf /etc/modprobe.d/999-legion-pm-fix-temp.conf
+# Install udev rule
+sudo install -Dm0644 data/udev-rule.rules /etc/udev/rules.d/999-legion-pm-fix-temp.rules
+# Install pm auto script
+sudo install -Dm0755 data/pm-auto-script.sh /var/lib/999-legion-pm-fix-temp/set-pm-auto.sh
+# Install systemd service
+sudo install -Dm0644 data/pm-service.service /etc/systemd/system/999-legion-pm-fix-temp.service
+# Enable service
+sudo systemctl enable 999-legion-pm-fix-temp.service
+
+echo "A reboot is required to finish installation"
+echo "Run systemctl reboot"
