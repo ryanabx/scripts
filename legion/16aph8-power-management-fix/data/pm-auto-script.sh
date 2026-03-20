bbcfb3444026c18376c49d69b34eaf2d6937ac55
Ryan Brue <ryanbrue.dev@gmail.com>
2026-03-20 11:21:18 -0500

    Power management fix: Simplify installer

--- legion/16aph8-power-management-fix/data/pm-auto-script.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-# Set nvidia runtime power management to auto
-echo auto > /sys/bus/pci/devices/0000:01:00.0/power/control
--- legion/16aph8-power-management-fix/install.sh
+++ legion/16aph8-power-management-fix/install.sh
@@ -1,16 +1,45 @@
 #!/bin/sh
 # https://github.com/NVIDIA/open-gpu-kernel-modules/issues/905#issuecomment-3360970555
 
+set -e
+
+echo "Installing NVIDIA PM fix files..."
+
 # Install modprobe config
-sudo install -Dm0644 data/modprobe-config.conf /etc/modprobe.d/999-legion-pm-fix-temp.conf
+sudo install -Dm0644 /dev/stdin /etc/modprobe.d/999-legion-pm-fix-temp.conf <<'EOF'
+options nvidia NVreg_DynamicPowerManagement=0x02
+options nvidia NVreg_DynamicPowerManagementVideoMemoryThreshold=0
+EOF
+
 # Install udev rule
-sudo install -Dm0644 data/udev-rule.rules /etc/udev/rules.d/999-legion-pm-fix-temp.rules
-# Install pm auto script
-sudo install -Dm0755 data/pm-auto-script.sh /usr/local/lib/999-legion-pm-fix-temp/set-pm-auto.sh
+sudo install -Dm0644 /dev/stdin /etc/udev/rules.d/999-legion-pm-fix-temp.rules <<'EOF'
+# Disable runtime PM for NVIDIA VGA/3D controller devices on driver bind
+ACTION=="bind", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x030000", TEST=="power/control", ATTR{power/control}="on"
+ACTION=="bind", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x030200", TEST=="power/control", ATTR{power/control}="on"
+
+# Disable runtime PM for NVIDIA VGA/3D controller devices on driver unbind
+ACTION=="unbind", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x030000", TEST=="power/control", ATTR{power/control}="on"
+ACTION=="unbind", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x030200", TEST=="power/control", ATTR{power/control}="on"
+EOF
+
 # Install systemd service
-sudo install -Dm0644 data/pm-service.service /etc/systemd/system/999-legion-pm-fix-temp.service
-# Enable service
+sudo install -Dm0644 /dev/stdin /etc/systemd/system/999-legion-pm-fix-temp.service <<'EOF'
+[Unit]
+Description=Enable Nvidia GPU runtime PM
+After=graphical.target
+
+[Service]
+Type=oneshot
+ExecStart=/bin/sh -c 'echo auto > /sys/bus/pci/devices/0000:01:00.0/power/control'
+
+[Install]
+WantedBy=graphical.target
+EOF
+
+# Reload systemd and enable service
+sudo systemctl daemon-reexec
+sudo systemctl daemon-reload
 sudo systemctl enable 999-legion-pm-fix-temp.service
 
 echo "A reboot is required to finish installation"
-echo "Run systemctl reboot"
+echo "Run: systemctl reboot"
